e3cca23bc96da2bea8d5ecbf7830885cfd4c992d
chigari101 <chigari101@gmail.com>
2023-03-24 21:16:43 +0000

    added files on AirBnB_clone_v2 project

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    """Represent an amenity.
-    """
-
-    name = ""
+class Amenity(BaseModel, Base):
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
